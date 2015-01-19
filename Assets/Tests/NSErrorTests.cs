f480767af5d4ef336c83a8a8920e45f4d35f37de
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-01-19 16:10:49 -0600

    More NSError implementation

--- Assets/Tests/NSErrorTests.cs
+++ Assets/Tests/NSErrorTests.cs
@@ -20,4 +20,27 @@ public class NSErrorTests
         var obj = new NSError("com.hitcents.ios4unity", 122);
         obj.Dispose();
     }
+
+    [Test]
+    public void Code()
+    {
+        int code = 122;
+        var error = new NSError("com.hitcents.ios4unity", code);
+        Assert.AreEqual(code, error.Code);
+    }
+
+    [Test]
+    public void Domain()
+    {
+        string domain = "com.hitcents.ios4unity";
+        var error = new NSError(domain, 122);
+        Assert.AreEqual(domain, error.Domain);
+    }
+
+    [Test]
+    public void LocalizedDescription()
+    {
+        var error = new NSError("UNKNOWN", -1);
+        Assert.AreEqual("The operation couldn’t be completed. (UNKNOWN error -1.)", error.LocalizedDescription);
+    }
 }
