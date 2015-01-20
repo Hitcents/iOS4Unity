4cb7bec225e9be016106ad95d20373948d827da7
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-01-20 08:44:27 -0600

    Added UIApplication

---
+++ Assets/Tests/UIApplicationTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using iOS4Unity;
+using NUnit.Framework;
+
+#if !UNITY_EDITOR
+
+public class UIApplicationTests 
+{
+    [Test]
+    public void SharedApplication()
+    {
+        var app = UIApplication.SharedApplication;
+
+        Assert.AreNotEqual(IntPtr.Zero, app.Handle);
+    }
+
+    [Test]
+    public void DefaultCenterDispose()
+    {
+        var app = UIApplication.SharedApplication;
+        app.Dispose();
+    }
+
+    [Test]
+    public void KeyWindow()
+    {
+        var window = UIApplication.SharedApplication.KeyWindow;
+        Assert.IsNotNull(window);
+        Assert.AreNotEqual(IntPtr.Zero, window.Handle);
+    }
+
+    [Test]
+    public void BadgeNumber()
+    {
+        //Just make sure this doesn't crash
+        var app = UIApplication.SharedApplication;
+        app.ApplicationIconBadgeNumber = 1;
+        int number = app.ApplicationIconBadgeNumber;
+    }
+
+    [Test]
+    public void ApplicationState()
+    {
+        Assert.AreEqual(UIApplicationState.Active, UIApplication.SharedApplication.ApplicationState);
+    }
+
+    [Test]
+    public void Windows()
+    {
+        var windows = UIApplication.SharedApplication.Windows;
+        Assert.IsNotNull(windows);
+        Assert.AreNotEqual(0, windows.Length);
+    }
+
+    [Test]
+    public void CanOpenUrl()
+    {
+        Assert.IsTrue(UIApplication.SharedApplication.CanOpenUrl("http://google.com"));
+    }
+
+    [Test]
+    public void StatusBarHidden()
+    {
+        Assert.IsFalse(UIApplication.SharedApplication.StatusBarHidden);
+    }
+
+    [Test]
+    public void StatusBarStyle()
+    {
+        Assert.AreEqual(UIStatusBarStyle.Default, UIApplication.SharedApplication.StatusBarStyle);
+    }
+
+    [Test]
+    public void NetworkActivityIndicatorVisible()
+    {
+        Assert.IsFalse(UIApplication.SharedApplication.NetworkActivityIndicatorVisible);
+    }
+
+    [Test]
+    public void IdleTimerDisabled()
+    {
+        Assert.IsFalse(UIApplication.SharedApplication.IdleTimerDisabled);
+    }
+}
+
+#endif
--- Assets/iOS4Unity/ObjC.cs
+++ Assets/iOS4Unity/ObjC.cs
@@ -15,6 +15,7 @@ namespace iOS4Unity
             { typeof(UIScreenMode), h => new UIScreenMode(h) },
             { typeof(UIScreen), h => new UIScreen(h) },
             { typeof(UIView), h => new UIView(h) },
+            { typeof(UIWindow), h => new UIWindow(h) },
         };
 
 		[DllImport("/usr/lib/libobjc.dylib", EntryPoint = "sel_registerName")]
@@ -73,6 +74,9 @@ namespace iOS4Unity
 
 		[DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
 		public static extern void MessageSend(IntPtr receiver, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SelectorMarshaler))] string selector, int arg1, bool arg2);
+
+        [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+        public static extern void MessageSend(IntPtr receiver, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SelectorMarshaler))] string selector, bool arg1, bool arg2);
 
         [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
         public static extern uint MessageSendUInt(IntPtr receiver, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SelectorMarshaler))] string selector);
@@ -141,6 +145,9 @@ namespace iOS4Unity
 
         [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
         public static extern IntPtr MessageSendIntPtr_NSUrl(IntPtr receiver, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SelectorMarshaler))] string selector, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(NSUrlMarshaler))] string arg1, uint arg2, out IntPtr arg3);
+
+        [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+        public static extern bool MessageSendBool_NSUrl(IntPtr receiver, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SelectorMarshaler))] string selector, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(NSUrlMarshaler))] string arg1);
 
         [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend_stret")]
         public static extern CGRect MessageSendCGRect(IntPtr receiver, [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SelectorMarshaler))] string selector);
