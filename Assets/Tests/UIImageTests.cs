3153dba5ea6002068b29c33d826f6335e00dd742
Nick Peppers <nick.peppers@hitcents.com>
2015-01-20 10:30:13 -0600

    Added image for tests

--- Assets/Tests/UIImageTests.cs
+++ Assets/Tests/UIImageTests.cs
@@ -8,8 +8,39 @@ using NUnit.Framework;
 public class UIImageTests
 {
     [Test]
+    public void AsJPEG()
+    {
+    }
+
+    [Test]
+    public void AsPNG()
+    {
+    }
+
+    [Test]
+    public void FromBundle()
+    {
+        var image = UIImage.FromBundle("chuck.jpg");
+        Assert.IsNotNull(image);
+        Assert.AreNotEqual(0, image.Size.Height);
+        Assert.AreNotEqual(0, image.Size.Width);
+    }
+
+    [Test]
+    public void FromFile()
+    {
+        var image = UIImage.FromFile("chuck.jpg");
+        Assert.IsNotNull(image);
+        Assert.AreNotEqual(0, image.Size.Height);
+        Assert.AreNotEqual(0, image.Size.Width);
+    }
+
+    [Test]
     public void Size()
     {
+        var size = UIImage.FromFile("chuck.jpg").Size;
+        Assert.AreNotEqual(0, size.Height);
+        Assert.AreNotEqual(0, size.Width);
     }
 }
 
--- Assets/iOS4Unity/UIImage.cs
+++ Assets/iOS4Unity/UIImage.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections;
+using System.Runtime.InteropServices;
 
 namespace iOS4Unity
 {
     public class UIImage : NSObject
     {
+        [DllImport("/System/Library/Frameworks/UIKit.framework/UIKit")]
+        private static extern IntPtr UIImageJPEGRepresentation(IntPtr image, float compressionQuality);
+        [DllImport("/System/Library/Frameworks/UIKit.framework/UIKit")]
+        private static extern IntPtr UIImagePNGRepresentation(IntPtr image);
+
         private static readonly IntPtr _classHandle;
 
         static UIImage()
@@ -18,6 +24,21 @@ namespace iOS4Unity
         }
 
         internal UIImage(IntPtr handle) : base(handle) { }
+
+        public NSData AsJPEG(float compressionQuality = 1)
+        {
+            return new NSData(UIImageJPEGRepresentation(Handle, compressionQuality));
+        }
+
+        public NSData AsPNG()
+        {
+            return new NSData(UIImagePNGRepresentation(Handle));
+        }
+
+        public static UIImage FromBundle(string name)
+        {
+            return new UIImage(ObjC.MessageSendIntPtr(_classHandle, "imageNamed:", name));
+        }
 
         public static UIImage FromFile(string filename)
         {
