b7585412913582a27f46284c4f794e4f76119b55
Nick Peppers <nick.peppers@hitcents.com>
2015-01-20 16:47:40 -0600

    Added NSBundleTests

---
+++ Assets/Tests/NSBundleTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using iOS4Unity;
+
+#if !UNITY_EDITOR
+
+[TestFixture]
+public class NSBundleTests 
+{
+    [Test]
+    public void NSBundleTest()
+    {
+        var bundle = NSBundle.MainBundle;
+
+        Assert.AreNotEqual(IntPtr.Zero, bundle);
+    }
+
+    [Test]
+    public void MainScreenDispose()
+    {
+        var bundle = NSBundle.MainBundle;
+        bundle.Dispose();
+    }
+
+    [Test]
+    public void BundlePath()
+    {
+        var bundle = NSBundle.MainBundle;
+        Assert.AreNotSame(string.Empty, bundle.BundlePath);
+
+    }
+
+    [Test]
+    public void ResourcePath()
+    {
+        var bundle = NSBundle.MainBundle;
+        Assert.AreNotSame(string.Empty, bundle.ResourcePath);
+    }
+}
+
+#endif
