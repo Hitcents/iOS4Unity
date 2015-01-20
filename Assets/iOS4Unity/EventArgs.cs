dfd440d75668a74bca4a7ef2ad41e221cb2580c8
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-01-20 13:51:58 -0600

    HACK: fix where it wasn't building under Unity

--- Assets/iOS4Unity/EventArgs.cs
+++ Assets/iOS4Unity/EventArgs.cs
@@ -10,3 +10,10 @@ public class EventArgs<T1, T2> : EventArgs
 	public T1 Value1;
 	public T2 Value2;
 }
+
+#if !XAMARIN
+namespace System
+{
+	public delegate void Action<T1, T2, T3, T4, T5>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+}
+#endif
