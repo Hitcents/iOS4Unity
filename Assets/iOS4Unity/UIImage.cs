d10ef1cf9ff22f08e6e1b1e01d24617bf114bf0c
Nick Peppers <nick.peppers@hitcents.com>
2015-01-20 09:33:23 -0600

    Working on UIImage

---
+++ Assets/iOS4Unity/UIImage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace iOS4Unity
+{
+    public class UIImage : NSObject
+    {
+        private static readonly IntPtr _classHandle;
+
+        static UIImage()
+        {
+            _classHandle = ObjC.GetClass("UIImage");
+        }
+
+        public override IntPtr ClassHandle 
+        {
+            get { return _classHandle; }
+        }
+
+        internal UIImage(IntPtr handle) : base(handle) { }
+
+        public static UIImage FromFile(string filename)
+        {
+            return new UIImage(ObjC.MessageSendIntPtr(_classHandle, "imageWithContentsOfFile:", filename));
+        }
+    }
+}
