de4c372f09a045e013de2d4f5774adc6cae5850a
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-01-23 15:09:01 -0600

    Fixed mistake in build script

--- build.fsx
+++ build.fsx
@@ -9,8 +9,8 @@ open Fake.XamarinHelper
 
 let version = "1.0.0"
 let project = "iOS4Unity"
-let files = [|"Assets/Plugins/iOS4Unity/iOS4Unity.dll"; "Assets/Plugins/iOS4Unity/Examples.cs"; "Assets/Plugins/iOS4Unity/Examples.scene"|]
-let projectInUnity = "Assets/Plugins/" + project
+let files = [|"Assets/Plugins/iOS4Unity/iOS4Unity.dll"; "Assets/Plugins/iOS4Unity/Examples.cs"; "Assets/Plugins/iOS4Unity/Examples.unity"|]
+let projectInUnity = "Assets/" + project
 
 Target "clean" (fun () ->
     if not(Directory.Exists(projectInUnity)) then
