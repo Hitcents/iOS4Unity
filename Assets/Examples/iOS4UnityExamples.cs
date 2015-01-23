b873b966a5decca03cd4edc457aa43695304d420
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-01-23 10:34:36 -0600

    Improved examples

--- Assets/Examples/iOS4UnityExamples.cs
+++ Assets/Examples/iOS4UnityExamples.cs
@@ -70,4 +70,53 @@ public class iOS4UnityExamples : MonoBehaviour
     {
 		UIApplication.SharedApplication.OpenUrl("http://www.google.com");
     }
+
+	public void GetIAPPrices()
+	{
+		//NOTE: for this to work, your app's bundle ID should match what you have setup in iTunes Connect
+		//	Each in-app purchase ID should be what you have setup in iTunes Connect also
+		var request = new SKProductsRequest("com.yourcompany.iap1", "com.yourcompany.iap2");
+		request.Failed += (sender, e) => 
+		{
+			Debug.Log("Error retrieviing prices: " + e.Error.LocalizedDescription);
+		};
+		request.ReceivedResponse += (sender, e) => 
+		{
+			//Invalid ones -- this will print out by default
+			if (e.Response.InvalidProducts != null)
+			{
+				foreach (string invalidId in e.Response.InvalidProducts) 
+				{
+					Debug.Log ("Invalid ID: " + invalidId);
+				}
+			}
+
+			//Successful ones
+
+		};
+		request.Start();
+	}
+
+	private void PrintProducts(SKProduct[] products)
+	{
+		if (products == null)
+			return;
+
+		using (var formatter = new NSNumberFormatter())
+		{
+			formatter.FormatterBehavior = NSNumberFormatterBehavior.Version_10_4;
+			formatter.NumberStyle = NSNumberFormatterStyle.Currency;
+			
+			foreach (var product in products)
+			{
+				formatter.Locale = product.PriceLocale;
+
+				Debug.Log ("Identifier: " + product.ProductIdentifier);
+				//TODO: missing method!
+				//Debug.Log ("Price: " + formatter.StringFromNumber(product.Price));
+				Debug.Log ("Title: " + product.LocalizedTitle);
+				Debug.Log ("Description: " + product.LocalizedDescription);
+			}
+		}
+	}
 }
