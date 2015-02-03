614f46c4778cba4136b9e7e05147a714f10cceb2
Nick Peppers <nick.peppers@hitcents.com>
2015-02-03 15:30:27 -0600

    Updated NSDIctionary

--- Assets/iOS4Unity/NSDictionary.cs
+++ Assets/iOS4Unity/NSDictionary.cs
@@ -4,6 +4,18 @@ namespace iOS4Unity
 {
     public class NSDictionary : NSObject
     {
+        private static readonly IntPtr _classHandle;
 
+        static NSDictionary()
+        {
+            _classHandle = ObjC.GetClass("NSDictionary");
+        }
+
+        public override IntPtr ClassHandle 
+        {
+            get { return _classHandle; }
+        }
+
+        internal NSDictionary (IntPtr handle) : base(handle) { }
     }
 }
