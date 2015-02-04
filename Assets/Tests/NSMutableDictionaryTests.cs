dc85ebeec493ca0dbbd66501d2b9360ae8e166a6
Nick Peppers <nick.peppers@hitcents.com>
2015-02-04 10:04:51 -0600

    Working on NSMutableTests

---
+++ Assets/Tests/NSMutableDictionaryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using iOS4Unity;
+using NUnit.Framework;
+
+[TestFixture]
+public class NSMutableDictionaryTests
+{
+    [Test]
+    public void FromObjectAndKey()
+    {
+        string key = "WOOOTSDLKFJSDLKFDS:";
+        var value = new NSObject();
+        var dictionary = NSMutableDictionary.FromObjectAndKey(value, key);
+        var actual = dictionary.ObjectForKey(key);
+        Assert.AreEqual(value.Handle, actual.Handle);
+        Assert.AreSame(value, actual);
+    }
+
+    [Test]
+    public void Indexer()
+    {
+        string key = "WOOOTSDLKFJSDLKFDS:";
+        var value = new NSObject();
+        var dictionary = NSMutableDictionary.FromObjectAndKey(value, key);
+        var actual = dictionary[key];
+        Assert.AreEqual(value.Handle, actual.Handle);
+        Assert.AreSame(value, actual);
+    }
+}
