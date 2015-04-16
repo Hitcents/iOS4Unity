cec3891bb11ce650b09c34d0b776ca516a8f601d
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-04-16 11:28:44 -0500

    Changes in build script to build test apps

--- build.fsx
+++ build.fsx
@@ -11,6 +11,8 @@ let version = "1.0.0"
 let project = "iOS4Unity"
 let projectInUnity = Path.Combine("Assets", project)
 let examples = Path.Combine("Assets", "Examples")
+let provisioningId = "eceff215-f35c-45a6-bed8-09bb562401e9"
+let provisioningName = "GenericInHouse"
 
 Target "clean" (fun () ->
     Exec "git" "reset --hard HEAD"
@@ -23,6 +25,33 @@ Target "dll" (fun () ->
     MSBuild output "Build" [ ("Configuration", "Release") ] [ csproj ] |> ignore
 )
 
+Target "ios-player" (fun () ->
+    Unity "-executeMethod BuildScript.iOS"
+)
+ 
+Target "ios64-player" (fun () ->
+    Unity "-executeMethod BuildScript.iOS64"
+)
+
+Target "ios-archive" (fun () ->
+    DeleteDir ("Scratch/Xcode/" + project + ".xarchive/")
+    Xcode ("-project Scratch/Xcode/Unity-iPhone.xcodeproj -scheme Unity-iPhone archive -archivePath Scratch/Xcode/" + project + " PROVISIONING_PROFILE=" + provisioningId)
+)
+
+Target "tests" (fun () ->
+    CreateDir "build"
+    DeleteFile ("build/" + project + ".ipa")
+    Xcode ("-exportArchive -archivePath Scratch/Xcode/" + project + ".xcarchive -exportPath build/" + project + ".ipa -exportProvisioningProfile " + provisioningName)
+    TeamCityHelper.PublishArtifact (project + ".ipa")
+)
+
+Target "tests-64" (fun () ->
+    CreateDir "build"
+    DeleteFile ("build/" + project + ".ipa")
+    Xcode ("-exportArchive -archivePath Scratch/Xcode/" + project + ".xcarchive -exportPath build/" + project + "-64.ipa -exportProvisioningProfile " + provisioningName)
+    TeamCityHelper.PublishArtifact (project + "-64.ipa")
+)
+
 Target "unity" (fun () ->
     CleanDir projectInUnity
     File.Copy(Path.Combine(project, "bin", "Release", project + ".dll"), Path.Combine(projectInUnity, project + ".dll"))
@@ -31,6 +60,8 @@ Target "unity" (fun () ->
     UnityPackage projectInUnity
 )
 
+"ios-archive" ==> "tests"
+"ios-archive" ==> "tests-64"
 "dll" ==> "unity"
 
 RunTarget()
