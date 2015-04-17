cbaf8fd1c79a27afc210e27cf2c060f10133c5e5
Nick Peppers <nick.peppers@hitcents.com>
2015-04-17 09:09:23 -0500

    Added GKInviteTests

---
+++ Assets/Tests/GKInviteTests.cs
@@ -0,0 +1,31 @@
+﻿using iOS4Unity;
+using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class GKInviteTests
+{
+    [Test]
+    public void NewObject()
+    {
+        var invite = new GKInvite();
+
+        Assert.AreNotEqual(IntPtr.Zero, invite.ClassHandle);
+        Assert.AreNotEqual(IntPtr.Zero, invite.Handle);
+    }
+
+    [Test]
+    public void NewObjectDispose()
+    {
+        var invite = new GKInvite();
+        invite.Dispose();
+    }
+
+    [Test]
+    public void ObjectSame()
+    {
+        var a = new GKInvite();
+        var b = Runtime.GetNSObject<GKInvite>(a.Handle);
+        Assert.AreSame(a, b);
+    }
+}
